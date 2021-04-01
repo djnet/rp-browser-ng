ccb7f6303d0e413d1d5e9b0c441691be79727cd6
djanto <djnet@club-internet.fr>
2021-04-01 14:59:16 +0200

    update html

--- src/app-example/app-example.module.ts
+++ src/app-example/app-example.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppExampleComponent } from './app-example.component';
+import { RpBrowserComponent } from './rp-browser/rp-browser.component';
 
 @NgModule({
   declarations: [
-    AppExampleComponent
+    AppExampleComponent,
+    RpBrowserComponent
   ],
   imports: [
     BrowserModule
